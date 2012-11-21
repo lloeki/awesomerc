94b48745d35677523fb35e77373847f7d30fdae3
Loic Nageleisen <loic.nageleisen@gmail.com>
2012-11-21 14:27:24 +0100

    zenburn

--- ll/vars.lua
+++ ll/vars.lua
@@ -1,6 +1,6 @@
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm"
