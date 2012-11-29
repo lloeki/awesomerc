2ca0f714101bfbf614e5f40a8baf80228a5a65b5
Loic Nageleisen <loic.nageleisen@gmail.com>
2012-11-29 14:17:04 +0100

    better debug notifications

--- ll/dbg.lua
+++ ll/dbg.lua
@@ -1,5 +1,32 @@
-function dbg(vars)
-    local text = ""
-    for i=1, #vars do text = text .. vars[i] .. " | " end
-    naughty.notify({ text = text, timeout = 0 })
+Dbg = {}
+
+function Dbg.table_keys(table)
+    local keys = {}
+    local n = 0
+    for k, v in pairs(var) do
+        n = n + 1
+        keys[n] = k
+    end
+    return keys
 end
+
+function Dbg.to_text(var)
+    if type(var) == 'table' then
+        text = "{ "
+        for k, v in pairs(var) do
+            text = text .. k .. ":" .. Dbg.to_text(v) .. " "
+        end
+        text = text .. "}"
+    else
+        text = var
+    end
+    return text
+end
+
+function Dbg.naughty(vars)
+    local text = ""
+    for i=1, #vars do text = text .. "| " .. Dbg.to_text(vars[i]) .. "\n" end
+    naughty.notify({ text = string.sub(text, 1, -2), timeout = 10 })
+end
+
+dbg = Dbg.naughty
